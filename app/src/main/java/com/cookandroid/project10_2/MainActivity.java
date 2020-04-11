cd50defd6d86794f0a6aca594643f43d7c0edac6
YeHo <zxc21322@naver.com>
2020-04-11 21:43:22 +0900

    Android Example 11_1

---
+++ app/src/main/java/com/cookandroid/project10_2/MainActivity.java
@@ -0,0 +1,52 @@
+package com.cookandroid.project10_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final int voteCount[] = new int[9];
+        for (int i = 0; i < 9; i++) {
+            voteCount[i] = 0;
+        }
+
+        ImageView image[] = new ImageView[9];
+        Integer imageId[] = {R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5, R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9};
+        final String imgName[] = {"사진1", "사진2", "사진3", "사진4", "사진5", "사진6", "사진7", "사진8", "사진9"};
+
+        for (int i = 0; i < imageId.length; i++) {
+            final int index;
+            index = i;
+            image[index] = (ImageView)findViewById(imageId[index]);
+            image[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    voteCount[index]++;
+                    Toast.makeText(getApplicationContext(), imgName[index] + " : 총" + voteCount[index] + "표", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        Button btnFinish = (Button)findViewById(R.id.btnResult1);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                intent.putExtra("VoteCount", voteCount);
+                intent.putExtra("ImageName", imgName);
+                startActivity(intent);
+            }
+        });
+    }
+}
